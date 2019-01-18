45cb6fc587cada244398d76e7bff2b0338d7b165
tristandalle <tristandalle@gmail.com>
2019-01-18 16:23:27 +0100

    tuto lior 3/4

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(ArticleRepository $repo)
+    {
+        $articles = $repo->findAll();
+
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles'        => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('blog/home.html.twig');
+    }
+
+    /**
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+    public function form(Article $article = null, Request $request, ObjectManager $manager)
+    {
+
+        if(!$article){
+            $article = new Article();
+        }
+        /*$form = $this->createFormBuilder($article)
+            ->add('title', TextType::Class,[
+                'attr' => [
+                    'placeholder' => "Titre de l'article"
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => [
+                    'placeholder' => "Contenu de l'article"
+                    ]
+                ])
+            ->add('image', TextType::Class, [
+                'attr' => [
+                    'placeholder' => "Image de l'article"
+                    ]
+                ])
+            ->getForm();*/
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        dump($article);
+
+        if($form->isSubmitted() && $form->isValid()){
+            if(!$article->getId()){
+                $article->setCreatedAt(new \DateTime());
+            }
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !==null
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(ArticleRepository $repo, $id)
+    {
+        /*$repo = $this->getDoctrine()->getRepository(Article::class);*/
+        $article = $repo->find($id);
+
+        return $this->render('blog/show.html.twig',[
+            'article' => $article
+        ]);
+    }
+
+}
---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+        $faker = \Faker\Factory::create('fr_FR');
+
+        //Créer 3 fausses catégories
+        for ($i = 1; $i <= 3; $i++) {
+            $category = new Category();
+                $category->setTitle($faker->sentence())
+                ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            //Créer entre 4 et 6 articles
+
+            for ($j = 1; $j <= mt_rand(4,6); $j++){
+
+
+                $article = new Article();
+
+                $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+                    $article->setTitle($faker->sentence())
+                    ->setContent($content)
+                    ->setImage($faker->imageUrl())
+                    ->setCreatedAt($faker->dateTimeBetween('-6months'))
+                    ->setCategory($category);
+
+                $manager->persist($article);
+
+                for ($k = 1; $k <= mt_rand(4,10); $k++){
+                    $comment = new Comment();
+                    $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+
+                    $now = new \DateTime();
+                    $interval = $now->diff($article->getCreatedAt());
+                    $days = $interval->days;
+                    $minimun = '-' .$days . 'days';
+                    $comment->setAuthor($faker->name())
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween($minimun))
+                            ->setArticle($article);
+
+                    $manager->persist($comment);
+                }
+            }
+        }
+
+
+
+        $manager->flush();
+    }
+}
